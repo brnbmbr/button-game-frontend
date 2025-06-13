17532bb9aaddd4e3a48765109422517b5ba9bc2a
Cen B <senbyjames@gmail.com>
2025-06-13 09:52:07 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*", // update to Vercel frontend URL in production
+    methods: ["GET", "POST"]
+  }
+});
+
+let lobbies = {};
+
+io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+
+  socket.on("createLobby", ({ keyphrase }) => {
+    lobbies[keyphrase] = {
+      host: socket.id,
+      players: [socket.id]
+    };
+    socket.join(keyphrase);
+    io.to(keyphrase).emit("joined", { players: lobbies[keyphrase].players });
+  });
+
+  socket.on("joinLobby", ({ keyphrase }) => {
+    if (lobbies[keyphrase]) {
+      lobbies[keyphrase].players.push(socket.id);
+      socket.join(keyphrase);
+      io.to(keyphrase).emit("joined", { players: lobbies[keyphrase].players });
+    }
+  });
+
+  socket.on("startGame", ({ keyphrase }) => {
+    io.to(keyphrase).emit("startCountdown");
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected:", socket.id);
+    // Cleanup logic can go here
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
